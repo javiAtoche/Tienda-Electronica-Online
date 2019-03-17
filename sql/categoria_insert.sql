d8acdd6a360efe9a9fdf4f04cebfe6d623bae13b
javiAtoche <javieratocheortega@gmail.com>
2019-03-18 00:36:40 +0100

    Revert "Revert "Revert "añadido el footer v1"""
    This reverts commit 4c2b9673524253b08c1a5fd27d80ca8152925a96.

--- sql/categoria_insert.sql
+++
@@ -1,6 +0,0 @@
--- describe tabla `categoria`
-DESCRIBE `tienda_online`.`categoria`;
-
--- insert into tabla `categoria`
-INSERT INTO `tienda_online`.`categoria` (`categ_id`, `categ_nombre`)
-	VALUES ();
